7f02716fdf5aada79d2347d4ab717770dfb20429
Vishwaraj Yadav <vishu7128@gmail.com>
2024-11-08 00:13:38 +0530

    chore: ProductDetails UI bug

--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -65,7 +65,7 @@ function ProductDetail() {
               />
               <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
                 <h2 className="text-sm title-font text-gray-500 tracking-widest">
-                  {product.name.split(" ")[0]}
+                  {product.name ? product.name.split(" ")[0] : ""}
                 </h2>
                 <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">
                   {product.name}
