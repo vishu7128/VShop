c79c75034e0f38a42eb187392e34f353b1a20684
Vishwaraj Yadav <vishu7128@gmail.com>
2024-11-14 23:59:42 +0530

    chore:  profile page

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -70,13 +70,12 @@ function Profile() {
               </button>
 
               {/* Log Out Button */}
-              <a
+              <button
                 onClick={logoutHandler}
-                href="#"
                 className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-[#155E75] focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
               >
                 Log Out
-              </a>
+              </button>
             </div>
           )}
 
